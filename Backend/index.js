5f866f99e7849dd6f5bb635150dc1a75af16fc92
ganesh <ganeshyadavstudies@gmail.com>
2023-11-13 09:57:46 +0530

    Added

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,11 @@
 const express=require("express")
 const mongoose=require("mongoose")
+const dotenv=require("dotenv")
+
 const cors=require("cors")
 const userRouter = require("./router/userRouter")
 const authMiddleware = require("./middleware/Authmiddleware")
-
+dotenv.config()
 const app=express()
 app.use(cors())
 app.use(express.json())
@@ -22,7 +24,7 @@ app.use(authMiddleware)
 
 app.listen(8080,async()=>{
     try {
-        mongoose.connect("mongodb+srv://Ganesh:Yadav@cluster0.z7f4ecg.mongodb.net/TradingApp?retryWrites=true&w=majority")
+        mongoose.connect(process.env.URL)
         console.log("Server is listening at port 8080")
     } catch (error) {
         console.log("server error")
